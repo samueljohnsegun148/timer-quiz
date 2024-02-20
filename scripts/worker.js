7e296243988510969523c9e0a9faefd75f314449
samueljohnsegun148 <samueljohnsegun148@gmail.com>
2024-02-20 22:39:48 +0100

    Add getQuestion function

--- scripts/worker.js
+++ scripts/worker.js
@@ -35,3 +35,30 @@ function startQuiz() {
     getQuestion();
   }
   
+  
+function getQuestion() {
+    // get current question object from array
+    let currentQuestion = questions[currentQuestionIndex];
+    // update title with current question
+    questionTl.innerHTML = currentQuestion.title;
+    // clear out any old question choices
+    // loop over the choices for each question
+    // get the number of questions
+    let numberOfQuestions = questions.length; // assign it the value of the length of the questions array
+    let choicesContainer = [];
+  
+   for (choice of currentQuestion.choices) {
+      // create a new button for each choice, setting the label and value for the button
+      choicesContainer.push(
+        `<div>
+          <label>
+            <input type="radio" value="${choice}" name="answer${currentQuestionIndex}">
+              ${choice}
+          </label>
+        </div>`
+      );
+    }
+      // display the choice button on the page *
+      document.getElementById('choices').innerHTML = choicesContainer.join('');
+    }
+  
